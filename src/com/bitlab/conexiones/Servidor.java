8145ee3dc4542823aff0640f197c87a2fb3935c4
henrycallejas <henry.callejas@gmail.com>
2020-07-02 21:33:19 -0600

    Funcion principal agregada

--- src/com/bitlab/conexiones/Servidor.java
+++ src/com/bitlab/conexiones/Servidor.java
@@ -13,13 +13,16 @@ import java.util.logging.Logger;
  */
 public class Servidor {
     private static final int PUERTO = 8888;
+    private static Logger log = Logger.getLogger(Servidor.class.getName());
     
     public static void main(String[] args) {
         try {
             ServerSocket serverSocket = new ServerSocket(PUERTO); //Creo un nuevo socket con el puerto especificado
             while(true){
+                log.info("Esperando conexion de algun cliente");
                 Socket socket = serverSocket.accept(); //El socket esta pendiente de alguna conexion y cuando entra la acepta
                 HiloAntiendeClientes hilo = new HiloAntiendeClientes(socket); //Creo un nuevo hilo con la clase que hereda de Thread
+                System.out.println("Cliente " +socket.getInetAddress().getHostAddress() + " conectado exitosamente");
                 hilo.start(); //Inicia el nuevo hilo
             }
         } catch (IOException ex) {
